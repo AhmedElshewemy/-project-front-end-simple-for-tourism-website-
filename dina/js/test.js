3cdfbca0e2dba7e826ae29eb583adb93ea8eab23
Ahmed Elshewemy <96390565+AhmedElshewemy@users.noreply.github.com>
2023-01-28 07:38:12 +0200

    Add files via upload

---
+++ dina/js/test.js
@@ -0,0 +1,81 @@
+/*
+ 
+     <div> <img src="dina.jpg" width="200px"height= "200px"></div>
+     <a href="https://www.google.com" target="_blank">Google</a><br>
+     <a href="https://www.youtube.com" target="_blank"> Youtube</a><br>
+     <span class="you">kgrjou frpfokrvrjtih swopeirifr dlkdiorfh5</span><br>
+     <span class="me">kgrjou frpfokrvrjtih swopeirifr dlkdiorfh5</span><br>
+     <span class="yu">kgrjou frpfokrvrjtih swopeirifr dlkdiorfh5</span><br>
+     <span class="yo"> KGRJOU FRPFOKRVRJTIH SWOPEIRIFR DLKDIORFH5</span><br>
+     <img src="dina.jpg"width="50"height= "50">
+     div
+{
+    width:200px ;
+    height:200px;
+    background: rgb(239, 242, 245);
+    line-height:200%;
+    color:rgb(90, 79, 79) ;
+     /*white-space:normal;
+     text-indent:10px ;
+     div
+{
+div
+{
+    width:200px ;
+    height:200px;
+    background: rgb(239, 242, 245);
+    line-height:200%;
+    color:rgb(90, 79, 79) ;
+     /*white-space:normal;
+     text-indent:10px ;
+     */ 
+/*
+    }
+    div span
+    {
+        color:rgb(21, 239, 255) ;
+         text-indent:3px ;
+    }
+    span.you
+    {
+        text-transform: capitalize;
+        color:rgb(150, 9, 79) ;
+    
+    }
+    span.me
+    {
+        text-transform:uppercase ;
+        color:rgb(63, 28, 46) ;
+    
+    }
+    span.yu
+    {
+        text-transform:initial;
+        color:rgb(147, 214, 108) ;
+    
+    }
+    span.yo
+    {
+        text-transform :lowercase;
+        color:rgb(197, 153, 175) ;
+    
+    }*/
+
+
+
+
+
+/*
+    width:200px ;
+    height:200px;
+    background: rgb(239, 242, 245);
+    line-height:200%;
+    color:rgb(90, 79, 79) ;
+     /*white-space:normal;
+     text-indent:10px ;
+*/ function Home()
+{
+    "use strict";
+    window.location.herf="Home.html"
+}
+ 
